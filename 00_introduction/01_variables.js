320741d65d9ceb6e9390f24c4edf24b9d4848161
Jordan Darlington <darlodox@code-blue.local>
2022-08-18 21:30:22 +0100

    added variables

---
+++ 00_introduction/01_variables.js
@@ -0,0 +1,16 @@
+// VARIABLES
+
+let variableOne = 1;
+// 'let' is used to declare a block-scoped local variable
+variableOne = "one";
+// 'let' variables can have their value updated
+
+const variableTwo = 2;
+// 'const' is used to declare a block-scoped constant
+// constants cannot have their value updated
+
+var variableThree = 3;
+// 'var' is used to declare a globally-scoped variable
+// 'var' is often found in legacy code
+variableThree = "three";
+// 'var' variables can have their value updated
